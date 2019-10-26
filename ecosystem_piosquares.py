e5be7b376e4e9767b628a307951988c3ad6a2b22
Denis <denis.morozov.work@gmail.com>
2019-10-26 16:44:48 +0200

    Reporting completed, need to add design to docx and  improve resolution of images

--- ecosystem_piosquares.py
+++ ecosystem_piosquares.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
-from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from matplotlib.gridspec import GridSpec
 import matplotlib.image as mpimg
 import numpy as np
 import pandas as pd
-from xml.etree import ElementTree
-import json, xlrd, csv, urllib.request
+import xlrd, csv, requests, shutil
 from docx import Document
 from docx.shared import Inches
+from PIL import Image
+from docx.enum.style import WD_STYLE_TYPE
+from docx.shared import Pt
 
 # Step 1 - Extract variables from Json file and create dictionaries
 # Convert from Excel to CSV, remove unneeded columns, only leave 1 industry
@@ -50,39 +52,51 @@ print(industries)
 #Step 2 - Plot the graphs, iterate dictionaries, get logos, input into table
 class graph():
     def boxes(self):        
-        fig = plt.figure(constrained_layout=True)
+        fig = plt.figure(constrained_layout=True, dpi=200)
         gs = GridSpec(2, 2, figure=fig)
         
         ax1 = fig.add_subplot(gs[0, 0], title = industries[0])
-        file.loc[file['Your industry'] == industries[0]]
-        self.image_placer(ax1)
+        industry = 0
+        self.image_placer(ax1, industry)
             
         ax2 = fig.add_subplot(gs[0, 1], title = industries[1])
-        file.loc[file['Your industry'] == industries[1]]
-        self.image_placer(ax2)
+        industry = 1
+        self.image_placer(ax2, industry)
 
         ax3 = fig.add_subplot(gs[1, 0], title = industries[2])
-        file.loc[file['Your industry'] == industries[2]]
-        self.image_placer(ax3)
+        industry = 2
+        self.image_placer(ax3, industry)
         
         ax4 = fig.add_subplot(gs[1, 1], title = industries[3])
-        file.loc[file['Your industry'] == industries[3]]
-        self.image_placer(ax4)
+        industry = 3
+        self.image_placer(ax4, industry)
         
-        fig.suptitle("Ecosystem Map")
+        #fig.suptitle("Ecosystem Map")
         for i, ax in enumerate(fig.axes):
             ax.tick_params(labelbottom=False, labelleft=False, bottom = False, left = False)
         plt.savefig('ecosystem_map.png',bbox_inches='tight')
         plt.show()
 
-    def image_placer(self, _):
-        x, y  = 0.2, 0.2
+    def image_placer(self, _, industry):
         count = 1
-        for i in file['Logo']:
-            urllib.request.urlretrieve(i, i in file['Your industry'] + str(count) + ".png")
-            arr_AS = mpimg.imread(industries[0] + str(count) + ".png")
-            imagebox = OffsetImage(arr_AS, zoom=0.2)
-            if count < 4:
+        new = file.loc[file['Your industry'] == industries[industry]]
+        basewidth = 35
+        for i in new['Logo']:
+            png = i.split('/', 1)[-1].replace('/', '')
+            r = requests.get(i, stream  = True)
+            with open(png, 'wb') as out_file:
+                shutil.copyfileobj(r.raw, out_file) 
+            del r
+            img = Image.open(png)
+            wpercent = (basewidth/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+            img.save(png) 
+            arr_AS = mpimg.imread(png)
+            imagebox = OffsetImage(arr_AS)
+            if count == 1:
+                x, y  = 0.2, 0.2
+            if count > 1 and count < 4:
                 x += 0.3
             if count  == 4:
                 x = 0.2
@@ -105,13 +119,14 @@ class graph():
 g = graph()
 g.boxes()
 
-# Step 3 - Create Text and input all files and text into Word document
+# Step 3 - Create Text and input all files and text into Word document   
 document = Document()
-
-p = document.add_paragraph()
-r = p.add_run()
-r.add_text('Ecosystem Report for {0} {1} {2} and {3}'.format(*industries))
-r.add_picture('ecosystem_map.jpg') 
-r.add_text(' do you like it?')
-
+document.add_heading('Ecosystem Report for {0}, {1}, {2} and {3}'.format(*industries))
+p1 = document.add_paragraph()
+p1.alignment = 1
+r = p1.add_run()
+r.add_picture('ecosystem_map.png') 
+p2 = document.add_paragraph()
+r = p2.add_run()
+r.add_text(' HOW DO YOU LIKE ME NOW, DAD?')
 document.save('demo.docx')
